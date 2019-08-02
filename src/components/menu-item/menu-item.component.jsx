25887f1bc005ada0e8b78c7b531f7e198c8c6a0a
Tadas Balsys <balsys.tadas@gmail.com>
2019-08-02 13:13:25 +0300

    Added Directory, MenuItem components. Refactored HomePage component.

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import "./menu-item.styles.scss"
+
+const MenuItem = ({ title, imageUrl, size }) => (
+    <div className={`${size} menu-item`}
+        style={{
+            backgroundImage: `url(${imageUrl}) `
+        }}>>
+            <div className='content'>
+            <h2 className='title'>{title.toUpperCase()}</h2>
+            <span className='subtitle'>Shop now</span>
+        </div>
+    </div>
+)
+
+export default MenuItem;
