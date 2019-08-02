e4528110013d7e3628790ffb1e20fcb820cb55ba
Tadas Balsys <balsys.tadas@gmail.com>
2019-08-02 22:45:57 +0300

    Installed react-router-dom. Updated index.js with BrowserRouter comp; App.js with Switch, Route; MenuItem to navigate withRouter HOC.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-
+import { Switch, Route } from 'react-router-dom'
 import HomePage from './pages/homepage.component';
 import './App.css'
 
+const HatsPage = () =>{
+  // three props from the react router dom - history, location, match
+  return (<h2>This is Hats Page</h2>)
+}
+
+// Switch component - prevents from rendering all Route components. When Route's is in Switch component, React finds Route component with the path which are needed, renders that Route component and stops rendering other Route components.(A <Switch> will iterate over all of its children <Route> elements and only render the first one that matches the current location. This helps when multiple route’s paths match the same pathname, when animating transitions between routes, and in identifying when no routes match the current location (so that you can render a “404” component).)
+// Any component that gets render by Route component, gets three props - history, location, match
 function App() {
   return (
-    <HomePage />
+    <Switch>
+      <Route exact path='/' component={HomePage}/>
+      <Route path='/shop/hats' component={HatsPage}/>
+      <Route path='' />
+    </Switch>
   );
 }
 
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
 
 import "./menu-item.styles.scss"
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`}>
-        <div
-            className='background-image'
-            style={{
-                backgroundImage: `url(${imageUrl}) `
-            }}
-        />
-        <div className='content'>
-            <h2 className='title'>{title.toUpperCase()}</h2>
-            <span className='subtitle'>SHOP NOW</span>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
+    return (
+        <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+            <div
+                className='background-image'
+                style={{
+                    backgroundImage: `url(${imageUrl}) `
+                }}
+            />
+            <div className='content'>
+                <h2 className='title'>{title.toUpperCase()}</h2>
+                <span className='subtitle'>SHOP NOW</span>
+            </div>
         </div>
-    </div>
-)
-
-export default MenuItem;
+    )
+}
+// Route component from react-router-dom passes three props to component which he renders.
+// And only that rendered component can access those props. But in this case,
+// those props need to be passed down in DOM three to children components Route > Homepage(gets history, match, location props) > Directory > MenuItem(here those props: history, match. location, are needed)
+// withRouter will pass updated match, location and history props to the wrapped component whenever it renders
+export default withRouter(MenuItem);
