97c268e3a73ae0e0fb66042f4e6f3aa290cdea89
Tadas Balsys <balsys.tadas@gmail.com>
2019-09-05 09:58:57 +0300

    Added CartDropdown and CartIcon;

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-import { auth } from './../../firebase/firebase.utils'
+import { auth } from './../../firebase/firebase.utils';
 
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import CartIcon from '../cart-icon/cart-icon.component';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
-import './header.styles.scss'
+import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
     <div className='header'>
@@ -24,7 +26,9 @@ const Header = ({ currentUser }) => (
                     <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div> :
                     <Link className='option' to='/sign' >SIGN IN</Link>
             }
+            <CartIcon />
         </div>
+        <CartDropdown />
     </div>
 )
 
