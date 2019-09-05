cf656e4ee25d3e983ea0c0d07a58d5173b4cdc71
Tadas Balsys <balsys.tadas@gmail.com>
2019-09-05 12:00:29 +0300

    Added CustomButton to CollectionItem; Refactored CustomButton component; Added style rules to CustomButton and CollectionItem.

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 import './custom-button.styles.scss'
 
 // children property is innerTEXT value which are passed when component is rendered, ex sign-in-component.jsx
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => {
+const CustomButton = ({ children, isGoogleSignIn, inverted,...otherProps }) => {
     return (
-        <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps} >
-            {children}
+        <button className={` ${inverted ? 'google-sign-in' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps} >
+        {children}
         </button>
     )
 }
