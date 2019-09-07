01af5cc157a6a2c88a8862531262821b7d6c87b8
Tadas Balsys <balsys.tadas@gmail.com>
2019-09-07 14:29:04 +0300

    Moved Directory component state to Redux - created directory reducer and selector.

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -5,6 +5,7 @@ import storage from 'redux-persist/lib/storage';
 
 import userReducer from '../redux/user/user.reducer';
 import cartReducer from '../redux/cart/cart.reducers';
+import directoryReducer from '../redux/directory/directory.reducers';
 
 // whitelist - in this case the only reducer which redux-persist will persist is cartReducer
 const persistConfig = {
@@ -15,7 +16,8 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer
 });
  
 export const persistedRootReducer = persistReducer(persistConfig, rootReducer)
