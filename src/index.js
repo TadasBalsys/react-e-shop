23242c449d6d8f94807197c4dab7915863b7c159
Tadas Balsys <balsys.tadas@gmail.com>
2019-09-07 12:27:26 +0300

    Installed redux-persist; Implemented persist configurations in store.js, root-reducer.js and index.js.

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
 import './index.css';
 import App from './App';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 import * as serviceWorker from './serviceWorker';
 
 // BrowserRouter component provides all methods from react-route-dom to App  
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
-            <App />
+            <PersistGate loading={null} persistor={persistor} >
+                <App />
+            </PersistGate>
         </BrowserRouter>
     </Provider>,
     document.getElementById('root'));
@@ -19,4 +23,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister() ;
+serviceWorker.unregister();
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,21 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+// storage let's to use window.localStorage
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from '../redux/user/user.reducer';
 import cartReducer from '../redux/cart/cart.reducers';
 
-export default combineReducers({
+// whitelist - in this case the only reducer which redux-persist will persist is cartReducer
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
+ 
+export const persistedRootReducer = persistReducer(persistConfig, rootReducer)
