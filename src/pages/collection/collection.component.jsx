5c63ee7dcf743a0d1745ff0ab3e455187e3921ab
Tadas Balsys <balsys.tadas@gmail.com>
2019-09-08 17:32:32 +0300

    Implemented shop selector which select collection by name; Refactored Collection component.

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -2,29 +2,32 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectShopCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionByName } from '../../redux/shop/shop.selectors';
 import { addItem } from '../../redux/cart/cart.actions';
- 
+
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
 import './collection.styles.scss';
 
-const mapStateToProps = createStructuredSelector({
-    collections: selectShopCollections
+const mapStateToProps = (state, ownProps) => createStructuredSelector({
+    collection: selectCollectionByName(ownProps.match.params.collectionId)
 })
 
 const mapDispatchToProps = dispatch => ({
     addItem: item => dispatch(addItem(item))
 })
 
-
-const CollectionPage = ({ match, collections, addItem }) => {
-    const selectedCollection = collections.find(collection => collection.routeName === match.params.collectionId)
+const CollectionPage = ({ collection, addItem }) => {
+    const { title, items } = collection;
     return (
-        <div className='collection-page'> 
-            {selectedCollection.items.map((item, i) => (
-                <CollectionItem key={i} item={item} addItem={addItem}/>
-            ))}
+        <div className='collection-page'>
+            <h2 className='title'>{title}</h2>
+            <div className='items'>
+                {items.map((item, i) => (
+                    <CollectionItem key={i} item={item} addItem={addItem} />
+                ))}
+
+            </div>
         </div>
     )
 }
